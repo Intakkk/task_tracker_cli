b99ea040714e1c8e30aac8c7faaf74ec38abd843
Intakkk <parretq@gmail.com>
2025-05-26 10:08:50 +0200

    add functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-import sys
 from datetime import datetime
+import shlex
 
 def add(task_name):
     try:
@@ -16,33 +16,75 @@ def add(task_name):
 
     return print(f"Task added successfully (ID: {task["id"]})")
 
-def list():
+def list(task_status = None):
     for task in tasks:
-        print(f"{task['id']} {task['name']} - status: {task['status']} (created at {task['createdAt']} and updated at {task['updatedAt']})")
+        if task_status is None or task["status"] == task_status:
+            print(f"{task['id']} {task['name']} - status: {task['status']} (created at {task['createdAt']} and updated at {task['updatedAt']})")
+        
     return
 
 def update(task_id, task_name):
-    for task in tasks:
-        if task["id"] == int(task_id):
-            task["name"] = task_name
-            task["updatedAt"] = timestamp
+    task = next((t for t in tasks if t["id"] == int(task_id)), None)
 
-            with open("tasks.json", "w", encoding="utf-8") as file:
-                json.dump(tasks, file, indent=2, ensure_ascii=False)
+    if task is None:
+        return print(f"No task found (ID: {task_id})") 
 
-            return print(f"Task updated successfully (ID: {task_id})")
-        else:
-            return print(f"No task found (ID: {task_id})")  
+    task["name"] = task_name
+    task["updatedAt"] = timestamp
 
-try:
-    with open("tasks.json", "r", encoding="utf-8") as file:
-        tasks = json.load(file)
-except (FileNotFoundError, json.JSONDecodeError):
-    tasks = []
+    with open("tasks.json", "w", encoding="utf-8") as file:
+        json.dump(tasks, file, indent=2, ensure_ascii=False)
 
-timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return print(f"Task name updated successfully (ID: {task_id})")
 
-args = sys.argv[1:]
-cmd = args[0]
+def delete(task_id):
+    task = next((t for t in tasks if t["id"] == int(task_id)), None)
 
-globals()[cmd](*args[1:])
+    if task is None:
+        return print(f"No task found (ID: {task_id})") 
+    
+    tasks.remove(task)
+
+    with open("tasks.json", "w", encoding="utf-8") as file:
+        json.dump(tasks, file, indent=2, ensure_ascii=False)
+
+    return print(f"Task deleted successfully (ID: {task_id})")
+
+def mark(task_status, task_id):
+    task = next((t for t in tasks if t["id"] == int(task_id)), None)
+
+    if task is None:
+        return print(f"No task found (ID: {task_id})")
+    
+    task["status"] = task_status
+    task["updatedAt"] = timestamp
+
+    with open("tasks.json", "w", encoding="utf-8") as file:
+        json.dump(tasks, file, indent=2, ensure_ascii=False)
+
+    return print(f"Task status updated successfully (ID: {task_id})")
+
+while True:
+    try:
+        with open("tasks.json", "r", encoding="utf-8") as file:
+            tasks = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        tasks = []
+
+    task_cli = input("task-cli ").strip()
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    if task_cli.lower() in {"exit", "quit"}:
+        break
+    
+    task_cli_split = shlex.split(task_cli)
+    task_cli_resplit = task_cli_split[0].split("-", 1)
+    cmd = task_cli_resplit[0]
+    args = task_cli_resplit[1:] + task_cli_split[1:]
+
+    try:
+        globals()[cmd](*args)
+    except KeyError:
+        print(f"Unknown command : {cmd}")
+    except TypeError as e:
+        print(f"Wrong arguments for '{cmd}' : {e}")
