3c3faf66872ac6d4bea7c18246da50ff100d79b7
Intakkk <parretq@gmail.com>
2025-05-26 16:29:23 +0200

    Final commit

--- task-cli.py
+++ task-cli.py
@@ -14,16 +14,30 @@ def add(task_name):
     with open("tasks.json", "w", encoding="utf-8") as file:
         json.dump(tasks, file, indent=2, ensure_ascii=False)
 
-    return print(f"Task added successfully (ID: {task["id"]})")
+    print(f"Task added successfully (ID: {task['id']})")
 
 def list(task_status = None):
+    if task_status not in {None, "todo", "done", "in-progress"}:
+        return print(f"Status {task_status} not valid")
+    
+    found = None
+
     for task in tasks:
         if task_status is None or task["status"] == task_status:
             print(f"{task['id']} {task['name']} - status: {task['status']} (created at {task['createdAt']} and updated at {task['updatedAt']})")
-        
+            found = True
+
+    if found is None:
+        print(f"List with status {task_status} is empty")
+
     return
 
 def update(task_id, task_name):
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        return print("First argument must be an integer")
+    
     task = next((t for t in tasks if t["id"] == int(task_id)), None)
 
     if task is None:
@@ -35,9 +49,14 @@ def update(task_id, task_name):
     with open("tasks.json", "w", encoding="utf-8") as file:
         json.dump(tasks, file, indent=2, ensure_ascii=False)
 
-    return print(f"Task name updated successfully (ID: {task_id})")
+    print(f"Task name updated successfully (ID: {task_id})")
 
 def delete(task_id):
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        return print("Argument must be an integer")
+    
     task = next((t for t in tasks if t["id"] == int(task_id)), None)
 
     if task is None:
@@ -48,9 +67,17 @@ def delete(task_id):
     with open("tasks.json", "w", encoding="utf-8") as file:
         json.dump(tasks, file, indent=2, ensure_ascii=False)
 
-    return print(f"Task deleted successfully (ID: {task_id})")
+    print(f"Task deleted successfully (ID: {task_id})")
 
 def mark(task_status, task_id):
+    if task_status not in {"todo", "done", "in-progress"}:
+        return print(f"Status {task_status} not valid")
+    
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        return print("Second argument must be an integer")
+    
     task = next((t for t in tasks if t["id"] == int(task_id)), None)
 
     if task is None:
@@ -62,22 +89,24 @@ def mark(task_status, task_id):
     with open("tasks.json", "w", encoding="utf-8") as file:
         json.dump(tasks, file, indent=2, ensure_ascii=False)
 
-    return print(f"Task status updated successfully (ID: {task_id})")
+    print(f"Task status updated successfully (ID: {task_id})")
 
-while True:
+while True: # keep asking an input while no "exit" or "quit"
     try:
         with open("tasks.json", "r", encoding="utf-8") as file:
             tasks = json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         tasks = []
+        with open("tasks.json", "w", encoding="utf-8") as file:
+            json.dump(tasks, file, indent=2, ensure_ascii=False)
 
-    task_cli = input("task-cli ").strip()
+    task_cli = input("task-cli ").strip() # input cleaned from weird added spaces
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    if task_cli.lower() in {"exit", "quit"}:
+    if task_cli.lower() in {"exit", "quit"}: # breaks whenever input is "exit" or "quit"
         break
     
-    task_cli_split = shlex.split(task_cli)
+    task_cli_split = shlex.split(task_cli) # shlex does not split spaces between ""
     task_cli_resplit = task_cli_split[0].split("-", 1)
     cmd = task_cli_resplit[0]
     args = task_cli_resplit[1:] + task_cli_split[1:]
